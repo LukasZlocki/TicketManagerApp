18f5edafb85e79f37cfecde91d33859f0dd1b839
lzlocki <lzlocki@poczta.fm>
2024-09-08 14:55:09 +0200

    add test parameter service and implent into new ticket page

---
+++ TicketManagerApp/TicketManagerApp/Services/ITestParameterServices.cs
@@ -0,0 +1,9 @@
+﻿using TicketManager.Models.Models;
+
+namespace TicketManagerApp.Services.TestParameter_Services
+{
+    public interface ITestParameterServices
+    {
+        public Task<List<TestParameter>> GetTestParametersByTestId(int testId);
+    }
+}

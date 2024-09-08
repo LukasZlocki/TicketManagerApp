c0f75981add5a762253791145893c48672c924fe
lzlocki <lzlocki@poczta.fm>
2024-09-08 13:53:39 +0200

    add department service

---
+++ TicketManagerApp/TicketManagerApp/Services/DepartmentService.cs
@@ -0,0 +1,30 @@
+﻿
+using Microsoft.EntityFrameworkCore;
+using TicketManager.Models.Models;
+using TicketManagerApp.Data;
+
+namespace TicketManagerApp.Services.Department_Services
+{
+    public class DepartmentService : IDepartmentService
+    {
+        private readonly ApplicationDbContext _db;
+
+        public DepartmentService(ApplicationDbContext db)
+        {
+            _db = db;
+        }
+
+        /// <summary>
+        /// Returns list of departments object which are located in factory by factory primary key.
+        /// </summary>
+        /// <param name="factoryLocationId"></param>
+        /// <returns>List of Department</returns>
+        public async Task<List<Department>> GetAllDepartmentsByFactoryLocationId(int factoryLocationId)
+        {
+            var departments = await _db.Departments
+                .Where(id => id.FactoryLocationId == factoryLocationId)
+                    .ToListAsync();
+            return departments;
+        }
+    }
+}

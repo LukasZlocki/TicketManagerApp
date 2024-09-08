99ba179ead7e0069b839cd9fbc0e7881cbb7685c
lzlocki <lzlocki@poczta.fm>
2024-09-08 12:23:44 +0200

    perform first migration to db

--- TicketManagerApp/TicketManagerApp/Data/Migrations/20240908102256_initMigration.Designer.cs
+++ TicketManagerApp/TicketManagerApp/Data/Migrations/20240908102256_initMigration.Designer.cs
@@ -3,6 +3,7 @@ using System;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
 using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 using TicketManagerApp.Data;
 
@@ -11,9 +12,11 @@ using TicketManagerApp.Data;
 namespace TicketManagerApp.Migrations
 {
     [DbContext(typeof(ApplicationDbContext))]
-    partial class ApplicationDbContextModelSnapshot : ModelSnapshot
+    [Migration("20240908102256_initMigration")]
+    partial class initMigration
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        /// <inheritdoc />
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder

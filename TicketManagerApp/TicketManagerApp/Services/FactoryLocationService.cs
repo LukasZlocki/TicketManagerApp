786ee253ed11fff8031d84f05d1fdfb0572134e0
lzlocki <lzlocki@poczta.fm>
2024-09-08 13:16:17 +0200

    add factory location service

---
+++ TicketManagerApp/TicketManagerApp/Services/FactoryLocationService.cs
@@ -0,0 +1,26 @@
+﻿using Microsoft.EntityFrameworkCore;
+using TicketManager.Models.Models;
+using TicketManagerApp.Data;
+
+namespace TicketManagerApp.Services.FactoryLocation_Services
+{
+    public class FactoryLocationService : IFactoryLocationService
+    {
+        private readonly ApplicationDbContext _db;
+
+        public FactoryLocationService(ApplicationDbContext db)
+        {
+            _db = db;
+        }
+
+        /// <summary>
+        /// Returns all factory locations
+        /// </summary>
+        /// <returns>List of FactoryLocation objects</returns>
+        public async Task<List<FactoryLocation>> GetAllFactoryLocations()
+        {
+            var factoryLocations = await _db.FactoryLocations.ToListAsync();
+            return factoryLocations;
+        }
+    }
+}
---
+++ TicketManagerApp/TicketManagerApp/Services/IFactoryLocationService.cs
@@ -0,0 +1,9 @@
+﻿using TicketManager.Models.Models;
+
+namespace TicketManagerApp.Services.FactoryLocation_Services
+{
+    public interface IFactoryLocationService
+    {
+        public Task <List<FactoryLocation>> GetAllFactoryLocations();
+    }
+}

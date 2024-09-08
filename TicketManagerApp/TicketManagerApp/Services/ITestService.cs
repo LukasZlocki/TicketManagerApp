c49f3f0114d01afc224effb084f0124000abeda3
lzlocki <lzlocki@poczta.fm>
2024-09-08 14:11:41 +0200

    add test service

---
+++ TicketManagerApp/TicketManagerApp/Services/ITestService.cs
@@ -0,0 +1,9 @@
+﻿using TicketManager.Models.Models;
+
+namespace TicketManagerApp.Services.Test_Services
+{
+    public interface ITestService
+    {
+        public Task<List<Test>> GetTestsByLabLocation(int labLocationId);
+    }
+}
---
+++ TicketManagerApp/TicketManagerApp/Services/TestService.cs
@@ -0,0 +1,30 @@
+﻿using Microsoft.EntityFrameworkCore;
+using TicketManager.Models.Models;
+using TicketManagerApp.Data;
+using TicketManagerApp.Services.Test_Services;
+
+namespace TicketManagerApp.Services.Test_services
+{
+    public class TestService : ITestService
+    {
+        private readonly ApplicationDbContext _db;
+
+        public TestService(ApplicationDbContext db)
+        {
+            _db = db;
+        }
+
+        /// <summary>
+        /// Returns all tests related to lab locations by lab loaction primary key
+        /// </summary>
+        /// <param name="labLocationId"></param>
+        /// <returns></returns>
+        public async Task<List<Test>> GetTestsByLabLocation(int labLocationId)
+        {
+            var service = await _db.Tests
+                .Where(id => id.LabLocationId == labLocationId)
+                    .ToListAsync();
+            return service;
+        }
+    }
+}

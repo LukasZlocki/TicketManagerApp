08af62888dfc92a16a9774cff2c91003868626e1
lzlocki <lzlocki@poczta.fm>
2024-09-08 12:42:32 +0200

    add basic ticket service

---
+++ TicketManagerApp/TicketManagerApp/Services/ITicketService.cs
@@ -0,0 +1,9 @@
+﻿using TicketManager.Models.Models;
+
+namespace TicketManagerApp.Services.Ticket_Services
+{
+    public interface ITicketService
+    {
+        public Task<List<Ticket>> GetAllTickets();
+    }
+}
---
+++ TicketManagerApp/TicketManagerApp/Services/TicketService.cs
@@ -0,0 +1,23 @@
+﻿using Microsoft.EntityFrameworkCore;
+using TicketManager.Models.Models;
+using TicketManagerApp.Data;
+
+namespace TicketManagerApp.Services.Ticket_Services
+{
+    public class TicketService : ITicketService
+    {
+        private readonly ApplicationDbContext _db;
+
+        public TicketService(ApplicationDbContext db)
+        {
+            _db = db;
+        }
+
+        public async Task<List<Ticket>> GetAllTickets()
+        {
+            var tickets = await _db.Tickets.ToListAsync();
+            return tickets;
+        }
+
+    }
+}

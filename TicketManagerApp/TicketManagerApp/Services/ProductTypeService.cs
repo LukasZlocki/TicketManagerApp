b9ec2286c3d085747a53799c5f103c64cc35eeb7
lzlocki <lzlocki@poczta.fm>
2024-09-08 14:00:04 +0200

    add product family services. Add retriving product families to page

---
+++ TicketManagerApp/TicketManagerApp/Services/ProductTypeService.cs
@@ -0,0 +1,30 @@
+﻿using Microsoft.EntityFrameworkCore;
+using TicketManager.Models.Models;
+using TicketManagerApp.Data;
+using TicketManagerApp.Services.ProductTypeServices;
+
+namespace TicketManagerApp.Services.ProductType_Services
+{
+    public class ProductTypeService : IProductTypeService
+    {
+        private readonly ApplicationDbContext _db;
+
+        public ProductTypeService(ApplicationDbContext db)
+        {
+            _db = db;
+        }
+
+        /// <summary>
+        /// Returns product types by product family primary key
+        /// </summary>
+        /// <param name="familyId"></param>
+        /// <returns>List of ProductType objects</returns>
+        public async Task<List<ProductType>> GetAllProductTypesByFamilyId(int familyId)
+        {
+            var productTypes = await _db.ProductTypes
+                .Where(id => id.ProductFamilyId == familyId)
+                    .ToListAsync();
+            return productTypes;
+        }
+    }
+}

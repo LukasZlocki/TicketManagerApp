5d784ebef7728bc5a709aa981a124410c841f11f
lzlocki <lzlocki@poczta.fm>
2024-09-21 18:22:58 +0200

    add interface for retriving active tickets

--- TicketManagerApp/TicketManagerApp/Services/ITicketService.cs
+++ TicketManagerApp/TicketManagerApp/Services/ITicketService.cs
@@ -7,7 +7,9 @@ namespace TicketManagerApp.Services
         public Task<Ticket> CreateTicket(Ticket ticket);
         public Task<List<Ticket>> GetAllTickets();
         public Task<List<Ticket>> GetTicketsByLabLocation(int labLocationId);
+        public Task<List<Ticket>> GetActiveTicketsByLabLocation(int labLocationId);
         public Task<List<Ticket>> GetTicketsByUserEmail(string userEmail);
+        public Task<List<Ticket>> GetActiveTicketsByUserEmail(string userEmail);
         public Task<Ticket> GetTicketDetails(int ticketId);
     }
 }

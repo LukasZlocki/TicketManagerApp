74eb08251dd8615eecdff76f5127bdea8aac7b61
lzlocki <lzlocki@poczta.fm>
2024-09-21 18:33:46 +0200

    add logic to retrive active tickets by user email

--- TicketManagerApp/TicketManagerApp/Services/TicketService.cs
+++ TicketManagerApp/TicketManagerApp/Services/TicketService.cs
@@ -93,9 +93,27 @@ namespace TicketManagerApp.Services
             return activeTickets;
         }
 
-        public Task<List<Ticket>> GetActiveTicketsByUserEmail(string userEmail)
+        public async Task<List<Ticket>> GetActiveTicketsByUserEmail(string userEmail)
         {
-            throw new NotImplementedException();
+            var activeTickets = await _db.Tickets
+                .Include(t => t.TicketTests)
+                    .ThenInclude(t => t.TicketTestParameters)
+                        .ThenInclude(t => t.TestParameter)
+                    .Include(t => t.TicketTests)
+                    .ThenInclude(t => t.Test)
+                .Include(t => t.RequestorDepartment)
+                    .ThenInclude(t => t.Factorylocation)
+                .Include(t => t.LabLocation)
+                .Include(t => t.Product)
+                    .Include(t => t.Product.ProductFamily)
+                    .Include(t => t.Product.ProductDisplacement)
+                    .Include(t => t.Product.ProductType)
+                .Include(t => t.TicketStatus)
+                    .Where(s => s.TicketStatus.StatusDescription == "In Progress" ||
+                    s.TicketStatus.StatusDescription == "Waiting")
+                .Where(e => e.RequestorEmail == userEmail)
+                .ToListAsync();
+            return activeTickets;
         }
 
         public async Task<List<Ticket>> GetAllTickets()

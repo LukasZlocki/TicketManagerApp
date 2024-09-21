e0df16b8380476fb122879eea64b946d9e7258f2
lzlocki <lzlocki@poczta.fm>
2024-09-21 18:30:21 +0200

    add logic to retrive active tickets by lab location

--- TicketManagerApp/TicketManagerApp/Services/TicketService.cs
+++ TicketManagerApp/TicketManagerApp/Services/TicketService.cs
@@ -71,6 +71,33 @@ namespace TicketManagerApp.Services
             return ticket;
         }
 
+        public async Task<List<Ticket>> GetActiveTicketsByLabLocation(int labLocationId)
+        {
+            var activeTickets = await _db.Tickets
+                .Include(t => t.TicketTests)
+                    .ThenInclude(t => t.TicketTestParameters)
+                        .ThenInclude(t => t.TestParameter)
+                    .Include(t => t.TicketTests)
+                    .ThenInclude(t => t.Test)
+                .Include(t => t.RequestorDepartment)
+                    .ThenInclude(t => t.Factorylocation)
+                .Include(t => t.LabLocation)
+                .Include(t => t.Product)
+                    .Include(t => t.Product.ProductFamily)
+                    .Include(t => t.Product.ProductDisplacement)
+                    .Include(t => t.Product.ProductType)
+                .Include(t => t.TicketStatus)
+                    .Where(s => s.TicketStatus.StatusDescription == "In Progress" || 
+                    s.TicketStatus.StatusDescription == "Waiting")
+                .Where(id => id.LabLocationId == labLocationId).ToListAsync();
+            return activeTickets;
+        }
+
+        public Task<List<Ticket>> GetActiveTicketsByUserEmail(string userEmail)
+        {
+            throw new NotImplementedException();
+        }
+
         public async Task<List<Ticket>> GetAllTickets()
         {
             var tickets = await _db.Tickets.ToListAsync();

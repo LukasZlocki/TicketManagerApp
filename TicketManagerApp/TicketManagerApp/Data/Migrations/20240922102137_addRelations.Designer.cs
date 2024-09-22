f0fe22d50672f9cab8613126fb010917682ffcbf
lzlocki <lzlocki@poczta.fm>
2024-09-22 12:27:58 +0200

    perfom migration to db

--- TicketManagerApp/TicketManagerApp/Data/Migrations/20240922102137_addRelations.Designer.cs
+++ TicketManagerApp/TicketManagerApp/Data/Migrations/20240922102137_addRelations.Designer.cs
@@ -3,6 +3,7 @@ using System;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
 using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 using TicketManagerApp.Data;
 
@@ -11,9 +12,11 @@ using TicketManagerApp.Data;
 namespace TicketManagerApp.Migrations
 {
     [DbContext(typeof(ApplicationDbContext))]
-    partial class ApplicationDbContextModelSnapshot : ModelSnapshot
+    [Migration("20240922102137_addRelations")]
+    partial class addRelations
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        /// <inheritdoc />
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
---
+++ TicketManagerApp/TicketManagerApp/Data/Migrations/20240922102137_addRelations.cs
@@ -0,0 +1,90 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace TicketManagerApp.Migrations
+{
+    /// <inheritdoc />
+    public partial class addRelations : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropForeignKey(
+                name: "FK_TicketTestParameters_TestParameters_TestParameterId",
+                table: "TicketTestParameters");
+
+            migrationBuilder.DropForeignKey(
+                name: "FK_TicketTestParameters_TicketTests_TicketTestId",
+                table: "TicketTestParameters");
+
+            migrationBuilder.DropForeignKey(
+                name: "FK_TicketTests_Tests_TestId",
+                table: "TicketTests");
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_TicketTestParameters_TestParameters_TestParameterId",
+                table: "TicketTestParameters",
+                column: "TestParameterId",
+                principalTable: "TestParameters",
+                principalColumn: "TestParameterId",
+                onDelete: ReferentialAction.Restrict);
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_TicketTestParameters_TicketTests_TicketTestId",
+                table: "TicketTestParameters",
+                column: "TicketTestId",
+                principalTable: "TicketTests",
+                principalColumn: "TicketTestId",
+                onDelete: ReferentialAction.Restrict);
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_TicketTests_Tests_TestId",
+                table: "TicketTests",
+                column: "TestId",
+                principalTable: "Tests",
+                principalColumn: "TestId",
+                onDelete: ReferentialAction.Restrict);
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropForeignKey(
+                name: "FK_TicketTestParameters_TestParameters_TestParameterId",
+                table: "TicketTestParameters");
+
+            migrationBuilder.DropForeignKey(
+                name: "FK_TicketTestParameters_TicketTests_TicketTestId",
+                table: "TicketTestParameters");
+
+            migrationBuilder.DropForeignKey(
+                name: "FK_TicketTests_Tests_TestId",
+                table: "TicketTests");
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_TicketTestParameters_TestParameters_TestParameterId",
+                table: "TicketTestParameters",
+                column: "TestParameterId",
+                principalTable: "TestParameters",
+                principalColumn: "TestParameterId",
+                onDelete: ReferentialAction.Cascade);
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_TicketTestParameters_TicketTests_TicketTestId",
+                table: "TicketTestParameters",
+                column: "TicketTestId",
+                principalTable: "TicketTests",
+                principalColumn: "TicketTestId",
+                onDelete: ReferentialAction.Cascade);
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_TicketTests_Tests_TestId",
+                table: "TicketTests",
+                column: "TestId",
+                principalTable: "Tests",
+                principalColumn: "TestId",
+                onDelete: ReferentialAction.Cascade);
+        }
+    }
+}

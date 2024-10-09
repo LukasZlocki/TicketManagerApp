3ccc387dff1b8fa72ec414c61595575f8dcbbf19
lzlocki <lzlocki@poczta.fm>
2024-10-09 17:45:05 +0200

    add migration

---
+++ TicketManagerApp/TicketManagerApp/Data/Migrations/20241009154420_ticketResponsibilityByEmail.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace TicketManagerApp.Migrations
+{
+    /// <inheritdoc />
+    public partial class ticketResponsibilityByEmail : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "ResponsibleLabSpecialist",
+                table: "Tickets");
+
+            migrationBuilder.AddColumn<string>(
+                name: "ResponsibleEmail",
+                table: "Tickets",
+                type: "nvarchar(max)",
+                nullable: false,
+                defaultValue: "");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "ResponsibleEmail",
+                table: "Tickets");
+
+            migrationBuilder.AddColumn<Guid>(
+                name: "ResponsibleLabSpecialist",
+                table: "Tickets",
+                type: "uniqueidentifier",
+                nullable: false,
+                defaultValue: new Guid("00000000-0000-0000-0000-000000000000"));
+        }
+    }
+}

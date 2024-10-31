f86620d4139f374b069d42f88ff2bf2d032e4894
lzlocki <lzlocki@poczta.fm>
2024-10-31 20:41:11 +0100

    add server driv eservices and its interface

--- TicketManagerApp/TicketManagerApp/Services/IServerDriveService.cs
+++ TicketManagerApp/TicketManagerApp/Services/IServerDriveService.cs
@@ -4,5 +4,8 @@
     {
         public Task<bool> CreateReportTypeFolderStructure(int ticketId);
         public Task<List<(string FolderPath, string FileName)>> GetListOfFilesInFolder(List<string> foldersPath, string documentCodeNumber);
+        public Task SaveFileToFolder(Stream fileStream, string fileName, string targetFolderPath, string DocumentCodeNumber);
+        public Task DeleteFileFromFolder(string fileName, string targetFolderPath, string DocumentCodeNumber);
+        public string GetServerPathToDatabaseFolder();
     }
 }

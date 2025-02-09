886ae57128ab5980bc6e08b2e1c04f8fc883536f
lzlocki <lzlocki@poczta.fm>
2025-02-09 12:45:26 +0100

    update program file

--- TicketManagerApp/TicketManagerApp/Program.cs
+++ TicketManagerApp/TicketManagerApp/Program.cs
@@ -7,99 +7,91 @@ using TicketManagerApp.Components.Account;
 using TicketManagerApp.Data;
 using TicketManagerApp.Services;
 
-internal class Program
+var builder = WebApplication.CreateBuilder(args);
+
+// Add services to the container.
+builder.Services.AddRazorComponents()
+    .AddInteractiveServerComponents()
+    .AddInteractiveWebAssemblyComponents();
+
+builder.Services.AddCascadingAuthenticationState();
+builder.Services.AddScoped<IdentityUserAccessor>();
+builder.Services.AddScoped<IdentityRedirectManager>();
+builder.Services.AddScoped<AuthenticationStateProvider, PersistingRevalidatingAuthenticationStateProvider>();
+
+var connectionString = builder.Configuration.GetConnectionString("DefaultConnection") ?? throw new InvalidOperationException("Connection string 'DefaultConnection' not found.");
+builder.Services.AddDbContext<ApplicationDbContext>(options =>
+    options.UseSqlServer(connectionString));
+builder.Services.AddDatabaseDeveloperPageExceptionFilter();
+
+// Register seeder
+builder.Services.AddScoped<DbSeeder>();
+
+// Register services
+builder.Services.AddScoped<ITicketService, TicketService>();
+builder.Services.AddScoped<IDepartmentService, DepartmentService>();
+builder.Services.AddScoped<IFactoryLocationService, FactoryLocationService>();
+builder.Services.AddScoped<ILabLocationService, LabLocationService>();
+builder.Services.AddScoped<IProductDisplacementService, ProductDisplacementService>();
+builder.Services.AddScoped<IProductFamilyService, ProductFamilyService>();
+builder.Services.AddScoped<IProductTypeService, ProductTypeService>();
+builder.Services.AddScoped<ITestParameterServices, TestParameterServices>();
+builder.Services.AddScoped<ITestService, TestService>();
+builder.Services.AddScoped<ITicketStatusService, TicketStatusService>();
+builder.Services.AddScoped<IUserService, UserService>();
+builder.Services.AddScoped<IReportTypeService, ReportTypeService>();
+builder.Services.AddScoped<IReportStructureService, ReportStructureService>();
+builder.Services.AddScoped<IServerDriveService, ServerDriveService>();
+builder.Services.AddScoped<IProductService, ProductService>();
+builder.Services.AddScoped<IServerFileService, ServerFileService>();
+
+builder.Services.AddIdentity<ApplicationUser, IdentityRole>(options => options.SignIn.RequireConfirmedAccount = true)
+    .AddEntityFrameworkStores<ApplicationDbContext>()
+    .AddDefaultTokenProviders();
+
+// Service for error logger purpose
+builder.Services.AddLogging(config =>
 {
-    private static async Task Main(string[] args)
-    {
-        var builder = WebApplication.CreateBuilder(args);
+    config.AddConsole();
+});
 
-        // Add services to the container.
-        builder.Services.AddRazorComponents()
-            .AddInteractiveServerComponents()
-            .AddInteractiveWebAssemblyComponents();
+builder.Services.AddSingleton<IEmailSender<ApplicationUser>, IdentityNoOpEmailSender>();
 
-        builder.Services.AddCascadingAuthenticationState();
-        builder.Services.AddScoped<IdentityUserAccessor>();
-        builder.Services.AddScoped<IdentityRedirectManager>();
-        builder.Services.AddScoped<AuthenticationStateProvider, PersistingRevalidatingAuthenticationStateProvider>();
+// Register HttpClient
+builder.Services.AddHttpClient();
 
-        builder.Services.AddAuthentication(options =>
-            {
-                options.DefaultScheme = IdentityConstants.ApplicationScheme;
-                options.DefaultSignInScheme = IdentityConstants.ExternalScheme;
-            })
-            .AddIdentityCookies();
+var app = builder.Build();
 
-        var connectionString = builder.Configuration.GetConnectionString("DefaultConnection") ?? throw new InvalidOperationException("Connection string 'DefaultConnection' not found.");
-        builder.Services.AddDbContext<ApplicationDbContext>(options =>
-            options.UseSqlServer(connectionString));
-        builder.Services.AddDatabaseDeveloperPageExceptionFilter();
-
-        // Register seeder
-        builder.Services.AddScoped<DbSeeder>();
-
-        // Register services
-        builder.Services.AddScoped<ITicketService, TicketService>();
-        builder.Services.AddScoped<IDepartmentService, DepartmentService>();
-        builder.Services.AddScoped<IFactoryLocationService, FactoryLocationService>();
-        builder.Services.AddScoped<ILabLocationService, LabLocationService>();
-        builder.Services.AddScoped<IProductDisplacementService, ProductDisplacementService>();
-        builder.Services.AddScoped<IProductFamilyService, ProductFamilyService>();
-        builder.Services.AddScoped<IProductTypeService, ProductTypeService>();
-        builder.Services.AddScoped<ITestParameterServices, TestParameterServices>();
-        builder.Services.AddScoped<ITestService, TestService>();
-        builder.Services.AddScoped<ITicketStatusService, TicketStatusService>();
-        builder.Services.AddScoped<IUserService, UserService>();
-        builder.Services.AddScoped<IReportTypeService, ReportTypeService>();
-        builder.Services.AddScoped<IReportStructureService, ReportStructureService>();
-        builder.Services.AddScoped<IServerDriveService, ServerDriveService>();
-
-
-
-        builder.Services.AddIdentityCore<ApplicationUser>(options => options.SignIn.RequireConfirmedAccount = true)
-            .AddRoles<IdentityRole>()
-            .AddEntityFrameworkStores<ApplicationDbContext>()
-            .AddSignInManager()
-            .AddDefaultTokenProviders();
-
-        builder.Services.AddSingleton<IEmailSender<ApplicationUser>, IdentityNoOpEmailSender>();
-
-        // Register HttpClient
-        builder.Services.AddHttpClient();
-
-        var app = builder.Build();
-
-        // Configure the HTTP request pipeline.
-        if (app.Environment.IsDevelopment())
-        {
-            app.UseWebAssemblyDebugging();
-            app.UseMigrationsEndPoint();
-        }
-        else
-        {
-            app.UseExceptionHandler("/Error", createScopeForErrors: true);
-            // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
-            app.UseHsts();
-        }
-
-        // Seeding
-        var scope = app.Services.CreateScope();
-        var seeder = scope.ServiceProvider.GetRequiredService<DbSeeder>();
-        await seeder.Seed();
-
-        app.UseHttpsRedirection();
-
-        app.UseStaticFiles();
-        app.UseAntiforgery();
-
-        app.MapRazorComponents<App>()
-            .AddInteractiveServerRenderMode()
-            .AddInteractiveWebAssemblyRenderMode()
-            .AddAdditionalAssemblies(typeof(TicketManagerApp.Client._Imports).Assembly);
-
-        // Add additional endpoints required by the Identity /Account Razor components.
-        app.MapAdditionalIdentityEndpoints();
-
-        app.Run();
-    }
+// Configure the HTTP request pipeline.
+if (app.Environment.IsDevelopment())
+{
+    app.UseWebAssemblyDebugging();
+    app.UseMigrationsEndPoint();
 }
+else
+{
+    app.MapBlazorHub("base/path");
+    app.UseExceptionHandler("/Error", createScopeForErrors: true);
+    // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
+    app.UseHsts();
+}
+
+// Seeding
+var scope = app.Services.CreateScope();
+var seeder = scope.ServiceProvider.GetRequiredService<DbSeeder>();
+await seeder.Seed();
+
+//PMP 11/05/2024 Checking if this fixes app on server app.UseHttpsRedirection();
+
+app.UseStaticFiles();
+app.UseAntiforgery();
+
+app.MapRazorComponents<App>()
+    .AddInteractiveServerRenderMode()
+    .AddInteractiveWebAssemblyRenderMode()
+    .AddAdditionalAssemblies(typeof(TicketManagerApp.Client._Imports).Assembly);
+
+// Add additional endpoints required by the Identity /Account Razor components.
+app.MapAdditionalIdentityEndpoints();
+
+app.Run();

ec6d5c4dfcb064ab3c603f1e116263f876810d30
lzlocki <lzlocki@poczta.fm>
2025-02-21 20:30:15 +0100

    create js file to download file at browser

---
+++ TicketManagerApp/TicketManagerApp/wwwroot/js/JavaScriptDownload.js
@@ -0,0 +1,8 @@
+﻿window.downloadFileFromBytes = (fileName, bytesBase64) => {
+    const link = document.createElement('a');
+    link.href = `data:application/octet-stream;base64,${bytesBase64}`;
+    link.download = fileName;
+    document.body.appendChild(link);
+    link.click();
+    document.body.removeChild(link);
+};
